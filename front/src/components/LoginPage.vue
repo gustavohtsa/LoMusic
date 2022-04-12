a95f054130b288c1085811339613358a8dbab400
Lucas Silva <lucas.silva@tsaaviacao.com.br>
2022-04-12 17:59:00 -0300

    muda o nome do login

--- front/src/components/LoginPage.vue
+++ front/src/components/LoginPage.vue
@@ -9,32 +9,32 @@
     <meta name="generator" content="Hugo 0.84.0">
     <title>Login</title>
   </head>
-  <body class="text-center">
+<body class="text-center">
 
   <main class="form-signin">
-  <form>
-    <div class="text-center">
-      <img class="mb-4" src="/src/assets/spotify-brandlogo.png" alt="" width="100" height="100">
-      <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-    </div>
-    <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-      <label for="floatingInput">Email address</label>
-    </div>
-    <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-      <label for="floatingPassword">Password</label>
-    </div>
+    <form>
+      <div class="text-center">
+        <img class="mb-4" src="/src/assets/spotify-brandlogo.png" alt="" width="100" height="100">
+        <h1 class="h3 mb-3 fw-normal">LoMusic</h1>
+      </div>
+      <div class="form-floating">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Usuário</label>
+      </div>
+      <div class="form-floating">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">Senha</label>
+      </div>
 
-    <div class="checkbox mb-3">
-      <label>
-        <input type="checkbox" value="remember-me"> Remember me
-      </label>
-    </div>
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-    <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
-  </form>
-</main>
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> Lembre de mim
+        </label>
+      </div>
+      <button class="w-100 btn btn-lg btn-primary" type="submit" style="color: #1ED760">Login</button>
+      <p class="mt-5 mb-3 text-muted text-center">&copy; 2022</p>
+    </form>
+  </main>
     
 </body>
 </template>
