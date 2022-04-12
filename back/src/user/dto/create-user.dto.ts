03ca0d72f6026829e98d6d98cf6d7f074e272a1b
Gustavo Henrique <gustavohej@gmail.com>
2022-04-12 14:45:15 -0300

    criação resource user

---
+++ back/src/user/dto/create-user.dto.ts
@@ -0,0 +1,13 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  name?: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  password: string;
+}
